0364636529d9a2cb8b8523cc1986462348c9dbc3
teamDyer <brando123@utexas.edu>
2019-05-13 11:51:51 -0500

    master.py

--- master.py
+++ master.py
@@ -1,3 +1,32 @@
-from Tools_Xlsx_Writer import hello
+from Tools_Xlsx_Writer import generate
+from collections import OrderedDict
 
-hello()
+data = OrderedDict()
+
+data['run_01'] = {
+	'test_0001': {
+		'_name': './blah',
+		'GFLOPS': '894',
+		'Memory Time (ms)': '53'
+	},
+	'test_0002': {
+		'_name': './none',
+		'GFLOPS': '2145',
+		'Memory Time (ms)': '6541'
+	}
+}
+
+data['run_02'] = {
+	'test_0001': {
+		'_name': './blah',
+		'GFLOPS': '897',
+		'Memory Time (ms)': '49'
+	},
+	'test_0002': {
+		'_name': './none',
+		'GFLOPS': '2168',
+		'Memory Time (ms)': '6684'
+	}
+}
+
+generate(data)
