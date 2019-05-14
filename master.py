f935849d0e87de6c2d43cc662a67e081161766bb
teamDyer <brando123@utexas.edu>
2019-05-13 19:41:09 -0500

    bdyer: switched the master to call the parser instead of the summarizer

--- master.py
+++ master.py
@@ -1,32 +1,8 @@
-from Tools_Xlsx_Writer import generate
-from collections import OrderedDict
+"""
+"""
 
-data = OrderedDict()
+# import Tools_Xlsx_Writer 
+# Tools_Xlsx_Writer.example()
 
-data['run_01'] = {
-	'test_0001': {
-		'_name': './blah',
-		'GFLOPS': '894',
-		'Memory Time (ms)': '53'
-	},
-	'test_0002': {
-		'_name': './none',
-		'GFLOPS': '2145',
-		'Memory Time (ms)': '6541'
-	}
-}
-
-data['run_02'] = {
-	'test_0001': {
-		'_name': './blah',
-		'GFLOPS': '897',
-		'Memory Time (ms)': '49'
-	},
-	'test_0002': {
-		'_name': './none',
-		'GFLOPS': '2168',
-		'Memory Time (ms)': '6684'
-	}
-}
-
-generate(data)
+import ccvPerf_Parser
+ccvPerf_Parser.example()
