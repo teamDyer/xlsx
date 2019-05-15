e61ba09b1148bcc27d51c2f93b6ed0e5cc7dfdc3
teamDyer <brando123@utexas.edu>
2019-05-14 20:18:06 -0500

    bdyer: updated the parser to work on the entire directory, next step is to make it a function

--- lib/ccvPerf_Parser.py
+++ lib/ccvPerf_Parser.py
@@ -10,10 +10,9 @@ import os
 from collections import OrderedDict
 
 CublasConfig = OrderedDict( {
-        "@@@ isamax test" : {"isamax Test Result": 3},
-        "@@@@ total errors" : {"Total Errors" : 3},
-        "^^^^ CUDA" : {"CUDA - Elapsed Time (s)" : 5, "CUDA - Bandwidth (GB/s)" : 10},
-        "^^^^REF" : {"REF - Elapsed Time (s)" : 4, "REF - Bandwidth (GB/s)" : 8, "REF - Speedup" : 10}
+        "@@@@" : {"Test Name": 1, "Result": 3},
+        "^^^^ CUDA" : {"CUDA - Elapsed Time (s)" : 5, "CUDA - Bandwidth (GB/s)" : 10}
+        #"^^^^REF" : {"REF - Elapsed Time (s)" : 4, "REF - Bandwidth (GB/s)" : 8, "REF - Speedup" : 10}
     }
 )
 
@@ -22,13 +21,16 @@ repository = 'examples/cublas'
 def example():
     """
     """
-    run = OrderedDict()
-    testLogs = os.listdir(repository)
-    for testLog in sorted(testLogs):
-        file = repository + '/' + testLog
-        content = read(file)
-        results = grep(content)
-        run[os.path.basename(testLog)] = results
+    data = OrderedDict()
+    for iteration in os.listdir(repository):
+        run = OrderedDict()
+        testLogs = os.listdir(repository + "/" + iteration)
+        for testLog in sorted(testLogs):
+            file = repository + '/' + iteration + '/' + testLog
+            results = grep(file)
+            run[os.path.basename(testLog)] = results
+        data[iteration] = run
+    return data
 
 def read(path):
     """
@@ -38,12 +40,15 @@ def read(path):
     content =  [line.strip() for line in content]
     return content
 
-def grep(content):
+def grep(file):
     """
     """ 
     results = OrderedDict()
+    content = read(file)
     ##add any extra stuff here that will be with test log
-    results['ccv'] = {'Command': content[0], 'Exit Code': content[-1]}
+    results['Test'] = os.path.splitext(os.path.basename(file))[0]
+    results['Command Line'] = content[0]
+    results['Exit Code'] = content[-1]
     ##parse for each grep request in their order
     for grep in CublasConfig.keys():
         for line in content:
