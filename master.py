438dfd3de8aec755837aebcc294d1284e49defd4
teamDyer <brando123@utexas.edu>
2019-05-14 20:19:23 -0500

    bdyer: added support for the new xlsx reader that uses openpyxl

--- master.py
+++ master.py
@@ -1,8 +1,20 @@
 """
 """
 
-# import Tools_Xlsx_Writer 
-# Tools_Xlsx_Writer.example()
-
 import ccvPerf_Parser
-ccvPerf_Parser.example()
+parsed = ccvPerf_Parser.example()
+
+import Tools_Xlsx_Writer 
+Tools_Xlsx_Writer.sample(parsed)
+
+import Tools_Xlsx_Reader
+read = Tools_Xlsx_Reader.example()
+
+print('diffing')
+print('-------')
+
+value = { k : read[k] for k in set(read) - set(parsed) }
+print(value)
+
+print('-------')
+print('done')
